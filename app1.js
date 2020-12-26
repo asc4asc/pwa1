523bea8cb2881b29927e1781b26ab5d8667f6c66
Andreas <asc4asc@users.noreply.github.com>
2020-12-26 20:50:43 +0100

    Update app1.js

--- app1.js
+++ app1.js
@@ -1,6 +1,9 @@
 "use strict";  
 document.body.addEventListener("click", event => {
  if (event.target.nodeName == "BUTTON") {
+   var n=document.getElementById("ab").innerHTML;
+   n=n*1+1;  
+   document.getElementById("ab").innerHTML=n;
    console.log("Clicked", event.target.textContent);
  }
 });
