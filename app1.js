f396ea7a030b4d1d2bd00f8b8c79291d5613ff49
Andreas <asc4asc@users.noreply.github.com>
2020-12-26 20:45:20 +0100

    Update app1.js

--- app1.js
+++ app1.js
@@ -1,3 +1,10 @@
+"use strict";  
+document.body.addEventListener("click", event => {
+ if (event.target.nodeName == "BUTTON") {
+   console.log("Clicked", event.target.textContent);
+ }
+});
+
 function AddAbschnitt() {
 var n=document.getElementById("ab").innerHTML;
   if ( n*1 == 1 || n*1 == 2 ) { 
