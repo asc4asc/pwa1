ddf7de6abadba82182fa416376bcb534a9007877
Andreas <asc4asc@users.noreply.github.com>
2020-12-26 21:28:59 +0100

    Update app1.js

--- app1.js
+++ app1.js
@@ -4,16 +4,6 @@ document.body.addEventListener("click", event => {
    var n=document.getElementById("ab1").innerHTML;
    n=n*1+1;  
    document.getElementById("ab1").innerHTML=n;
-   console.log("Clicked", event.target.textContent);
+   /* console.log("Clicked", event.target.textContent); */
  }
 });
-
-function AddAbschnitt() {
-var n=document.getElementById("ab").innerHTML;
-  if ( n*1 == 1 || n*1 == 2 ) { 
-    n=n*1+1; 
-  } else {
-    n="S"  
-  }    
-  document.getElementById("ab").innerHTML=n;
-}
