ed84375a2e4ad77726aa62f402ca84616317b2ec
Andreas <asc4asc@users.noreply.github.com>
2020-12-27 11:29:08 +0100

    Update app1.js

--- app1.js
+++ app1.js
@@ -1,6 +1,6 @@
 "use strict";  
+let n=1;
 document.body.addEventListener("click", event => {
- let n=1;
  if (event.target.nodeName == "BUTTON") {
    /* var n=document.querySelector('BUTTON').innerHTML;*/
    n=n*1+1;  
