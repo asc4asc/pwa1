53ae6e57500fb0726553ae3b370878b1f7fd2cd8
Andreas <asc4asc@users.noreply.github.com>
2020-12-27 11:17:46 +0100

    Update app1.js

--- app1.js
+++ app1.js
@@ -1,7 +1,8 @@
 "use strict";  
+n=1;
 document.body.addEventListener("click", event => {
  if (event.target.nodeName == "BUTTON") {
-   var n=document.querySelector('BUTTON').innerHTML;
+   /* var n=document.querySelector('BUTTON').innerHTML;*/
    n=n*1+1;  
    document.querySelector('BUTTON').innerText = "n";
    /* console.log("Clicked", event.target.textContent); */
