ae2af1799fee744c164e31f44437b87c7ad5c9e9
Andreas <asc4asc@users.noreply.github.com>
2020-12-27 13:27:15 +0100

    Update app1.js

--- app1.js
+++ app1.js
@@ -1,24 +1,20 @@
 "use strict";  
 document.body.addEventListener("click", event => {
  if (event.target.nodeName == "BUTTON") {
-   let tmp=document.querySelector('BUTTON');
-   var n=tmp.innerHTML;
-   n=n*1+1;  
-   tmp.style.backgroundColor = "yellow";
-   tmp.innerText = n;
+   countButton();
    /* console.log("Clicked", event.target.textContent); */ /* Debug */
  }
 });
 
 class countButton {
   constructor() {    
-    this.s="";
+    /* momentan leer */
   }
   doit(i) {
-    //for (this.i=0; this.i<10; this.i++) { 
-      this.s = "Hello JavaS"+i+"<br>"+this.s; 
-      document.getElementById("demo").innerHTML = this.s;
-      console.log("Hello JavaScript!",i,"\n");
-    //}
+    let tmp=document.querySelector('BUTTON');
+    var n=tmp.innerHTML;
+    n=n*1+1;  
+    tmp.style.backgroundColor = "yellow";
+    tmp.innerText = n;
   }
 };     
