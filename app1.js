a5976e3e3ff09368c0a1a3d1e7432009530202b6
Andreas <asc4asc@users.noreply.github.com>
2020-12-27 11:50:02 +0100

    Update app1.js

--- app1.js
+++ app1.js
@@ -2,8 +2,8 @@
 let n=1;
 document.body.addEventListener("click", event => {
  if (event.target.nodeName == "BUTTON") {
-   document.querySelector("BUTTON").style.backgroundColor = "red";
-   /* var n=document.querySelector('BUTTON').innerHTML;*/
+   document.querySelector("BUTTON").style.backgroundColor = "yellow";
+   var n=document.querySelector('BUTTON').innerHTML;
    n=n*1+1;  
    document.querySelector('BUTTON').innerText = n;
    /* console.log("Clicked", event.target.textContent); */
