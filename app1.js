183fb3454c2ba289295e51b9e899024e9298c905
Andreas <asc4asc@users.noreply.github.com>
2020-12-28 16:53:45 +0100

    Update app1.js

--- app1.js
+++ app1.js
@@ -1,35 +1,17 @@
 "use strict";  
 
-class countButton {
-  constructor(button,maxcount=5) {    
-    this.tmp=document.querySelector(button);
-    this.n=this.tmp.innerHTML;
-    this.maxcount=maxcount;
+class clock {
+  constructor() {    
   }
   doit() {
-    if (this.n < this.maxcount-1) { 
-      this.tmp.style.backgroundColor = "yellow";
-    } else { 
-      this.tmp.style.backgroundColor = "red";
-    }     
-    this.n=this.n*1+1;
-    this.tmp.innerText = this.n;
   }
 };     
 
-var b1 = new countButton('BUTTON1');
-var b2 = new countButton('BUTTON2');
+let tmp=document.querySelector(body);
+tmp.style.backgroundColor = "red";
+
 document.body.addEventListener("click", event => {
  switch (event.target.nodeName)  {
-    case 'BUTTON1':
-      b1.doit();
-      break;
-    case 'BUTTON2':
-      b2.doit();
-      break;
-    default:
-      b1.doit();
-      b2.doit();
-      break;
+ tmp.style.backgroundColor = "green";   
  }    
 });
