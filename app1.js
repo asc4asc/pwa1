d9b2a5be20a5eb6fde80a01c11016a7154828571
Andreas <asc4asc@users.noreply.github.com>
2020-12-28 17:05:06 +0100

    Update app1.js

--- app1.js
+++ app1.js
@@ -4,6 +4,5 @@ let tmp=document.querySelector("body");
 tmp.style.backgroundColor = "red";
 
 document.body.addEventListener("click", event => {
-    tmp.style.backgroundColor = "green";   
- }   
+    tmp.style.backgroundColor = "green";    
 });
