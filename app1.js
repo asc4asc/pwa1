adf039d6a5f7308017b93d5f0dada1cc790626f1
Andreas <asc4asc@users.noreply.github.com>
2020-12-28 17:12:44 +0100

    Update app1.js

--- app1.js
+++ app1.js
@@ -1,8 +1,13 @@
 "use strict";  
 
 let tmp=document.querySelector("body");
-tmp.style.backgroundColor = "red";
+tmp.style.backgroundColor = "blue";
 
+let i=1;
 document.body.addEventListener("click", event => {
-    tmp.style.backgroundColor = "green";    
+  if (i == 1) {
+    tmp.style.backgroundColor = "green"; i=0; 
+  } else {
+    tmp.style.backgroundColor = "yellow"; i=1;
+  }
 });
