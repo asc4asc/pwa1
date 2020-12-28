7b1c6ab452c04f41edd57a3d9cf10f109fefda90
Andreas <asc4asc@users.noreply.github.com>
2020-12-28 17:01:20 +0100

    Update app1.js

--- app1.js
+++ app1.js
@@ -1,10 +1,10 @@
 "use strict";  
 
-let tmp=document.querySelector(body);
+let tmp=document.querySelector("body");
 tmp.style.backgroundColor = "red";
 
 document.body.addEventListener("click", event => {
- if (event.target.nodeName)  {
+ if (event.target.nodeName == "CLOCK")  {
    tmp.style.backgroundColor = "green";   
  }   
 });
