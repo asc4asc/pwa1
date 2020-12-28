e9da6fe8f46dd00742ba613f9471260735c3522c
Andreas <asc4asc@users.noreply.github.com>
2020-12-28 17:34:26 +0100

    Update app1.js

--- app1.js
+++ app1.js
@@ -8,6 +8,20 @@ document.body.addEventListener("click", event => {
   if (i == 1) {
     tmp.style.backgroundColor = "green"; i=0; 
   } else {
-    tmp.style.backgroundColor = "yellow"; i=1;
+    tmp.style.backgroundColor = "yellow"; i=1; 
+    openFullscreen();
   }
 });
+
+var elem = document.documentElement;
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) { /* Firefox */
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE/Edge */
+    elem.msRequestFullscreen();
+  }
+}
