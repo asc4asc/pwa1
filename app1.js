0b1ec47c235359f92d9501d1de4ce2db0c71bbb7
Andreas <asc4asc@users.noreply.github.com>
2020-12-28 18:26:56 +0100

    Update app1.js

--- app1.js
+++ app1.js
@@ -3,6 +3,26 @@
 let tmp=document.querySelector("body");
 tmp.style.backgroundColor = "blue";
 
+(function () {
+	function uhrzeit() {
+		var jetzt = new Date(),
+			h = jetzt.getHours(),
+			m = jetzt.getMinutes(),
+			s = jetzt.getSeconds();
+		m = fuehrendeNull(m);
+		s = fuehrendeNull(s);
+		document.getElementById('uhr1')
+			.innerHTML = h + ':' + m + ':' + s;
+		setTimeout(uhrzeit, 500);
+	}
+
+	function fuehrendeNull(zahl) {
+		zahl = (zahl < 10 ? '0' : '') + zahl;
+		return zahl;
+	}
+	document.addEventListener('DOMContentLoaded', uhrzeit);
+}());
+
 let i=1;
 document.body.addEventListener("click", event => {
   if (i == 1) {
